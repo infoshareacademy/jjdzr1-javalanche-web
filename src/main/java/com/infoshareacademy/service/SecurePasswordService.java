9a57d0d9c7bd202dc7eccd84173297805fdb30ff
karoltylenda <karoltylenda@gmail.com>
2020-09-17 12:23:14 +0200

    add jasyt, and SecurePasswordService

---
+++ src/main/java/com/infoshareacademy/service/SecurePasswordService.java
@@ -0,0 +1,22 @@
+package com.infoshareacademy.service;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+public class SecurePasswordService {
+
+    protected static final String PASS_ENCRYPT = "JAKIES_HASLO_DO_KODOWANIA";
+
+    public String encryptor(String password) {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(PASS_ENCRYPT);
+        String encryptedText = encryptor.encrypt(password);
+        return encryptedText;
+    }
+
+    public String decryptor(String password) {
+        StandardPBEStringEncryptor decryptor = new StandardPBEStringEncryptor();
+        decryptor.setPassword(PASS_ENCRYPT);
+        String decryptedText = decryptor.decrypt(password);
+        return decryptedText;
+    }
+}
