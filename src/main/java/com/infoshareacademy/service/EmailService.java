e376a30aef9b8ad067f08ea2695cda9cfb184504
karoltylenda <karoltylenda@gmail.com>
2020-09-22 13:51:33 +0200

    add email service

---
+++ src/main/java/com/infoshareacademy/service/EmailService.java
@@ -0,0 +1,40 @@
+package com.infoshareacademy.service;
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+public class EmailService {
+
+    public static void sendEmail(String host, String port,
+                                 final String userName, final String password,
+                                 String toAddress, String subject, String message) throws MessagingException, AddressException {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+
+        Session session = Session.getDefaultInstance(properties, authenticator);
+
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = {new InternetAddress(toAddress)};
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+        msg.setText(message);
+
+        Transport.send(msg);
+    }
+}
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: karol
+  Date: 22.09.2020
+  Time: 12:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+    <h3><%=request.getAttribute("Message")%></h3>
+</body>
+</html>
