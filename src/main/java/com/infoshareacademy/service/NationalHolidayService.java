9de87ce9f0729043fb9d2df192a7c40efbddbcc7
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-09-29 17:56:46 +0200

    NationalHolidaysService  class ready.

---
+++ src/main/java/com/infoshareacademy/service/NationalHolidayService.java
@@ -0,0 +1,29 @@
+package com.infoshareacademy.service;
+
+import com.infoshareacademy.api.Holidays;
+import com.infoshareacademy.api.HolidaysJsonData;
+import com.infoshareacademy.model.NationalHoliday;
+import com.infoshareacademy.repository.NationalHolidayRepository;
+
+import javax.ejb.Local;
+import javax.inject.Inject;
+import java.util.List;
+
+@Local
+public class NationalHolidayService {
+
+    @Inject
+    private NationalHolidayRepository nationalHolidayRepository;
+
+    private void transferNationalHolidaysFromJsonToDatabase(){
+        List<Holidays>jsonHolidays = HolidaysJsonData.returnOnlyHolidaysAsList();
+        NationalHoliday nationalHoliday;
+        for(Holidays holiday : jsonHolidays){
+            nationalHoliday = new NationalHoliday();
+            nationalHoliday.setName(holiday.getName());
+            nationalHoliday.setDescription(holiday.getDescription());
+            nationalHoliday.setHolidayDate(holiday.getHolidayDateInLocalDateFormat());
+            nationalHolidayRepository.create(nationalHoliday);
+        }
+    }
+}
