7448e07718894e02f208e2489f0bff9ee6a92f8a
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-10-08 12:35:30 +0200

    First test template created.

--- src/test/java/com/infoshareacademy/service/FormsServiceTest.java
+++ src/test/java/com/infoshareacademy/service/FormsServiceTest.java
@@ -1,4 +1,27 @@
 package com.infoshareacademy.service;
 
+import com.infoshareacademy.model.User;
+import com.infoshareacademy.repository.UserRepository;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.List;
+
 public class FormsServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @Test
+    void defaultTest(){
+        //given
+        UserRepository userReposito = new UserRepository();
+
+        //when
+        List<User> usersFromDatabase = userRepository.getAll();
+
+        //then
+
+
+    }
 }
