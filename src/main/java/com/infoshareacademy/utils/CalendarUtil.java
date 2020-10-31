2b8fede459f9c69e449c2aa41037ac8b57f90fa3
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-10-31 21:30:23 +0100

    CalendarUtil class created.

---
+++ src/main/java/com/infoshareacademy/utils/CalendarUtil.java
@@ -0,0 +1,19 @@
+package com.infoshareacademy.utils;
+
+import java.time.LocalDate;
+
+public class CalendarUtil {
+
+    public static LocalDate calculateMaximumPossibleDayForHoliday(String date){
+
+        //TODO count for weekends
+
+        //TODO count for national holidays
+
+        //TODO count for pending requests
+
+        //TODO count for days of left
+
+        return null;
+    }
+}
